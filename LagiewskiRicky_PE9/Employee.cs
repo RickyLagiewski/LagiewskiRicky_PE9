147f684e9191d643a6c068955a0f0234e5ae3f0f
Richard Lagiewski <rickylagiewski@gmail.com>
2018-09-24 00:18:46 -0400

    fixing this

---
+++ LagiewskiRicky_PE9/Employee.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagiewskiRicky_PE9
+{
+    class Employee
+    {
+        string name;
+        string address;
+        string jobTitle;
+        int employeeNumber;
+        int yearlySalary;
+        //constructor
+        public Employee()
+        {
+            name = "Employee";
+            address = "123 Main Street";
+            jobTitle = "Worker";
+            employeeNumber = 00000;
+            yearlySalary = 0;
+        }
+        //parameterized constructor
+        public Employee(string p_name, string p_address, string p_jobTitle, int p_employeeNumber, int p_yearlySalary)
+        {
+            name = p_name;
+            address = p_address;
+            jobTitle = p_jobTitle;
+            employeeNumber = p_employeeNumber;
+            yearlySalary = p_yearlySalary;
+        }
+        //method
+        public void ChangeName()
+        {
+            Console.WriteLine("\nWhat is the employee's name? ");
+            name = Console.ReadLine();
+        }
+        //method 
+        public void ChangeSalary()
+        {
+            Console.WriteLine("\nWhat is the new salary for the employee? ");
+            yearlySalary = Console.Read();
+        }
+        //method
+        public void PrintEmployee()
+        {
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Address: " + address);
+            Console.WriteLine("Job Title: " + jobTitle);
+            Console.WriteLine("Employee Number: " + employeeNumber);
+            Console.WriteLine("Yearly Salary: " + yearlySalary);
+        }
+
+    }
+}
---
+++ LagiewskiRicky_PE9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LagiewskiRicky_PE9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating object
+            Employee myCustomeEmployee = new Employee("Ricky", "123 XD", "Meme lord", 69, 12);
+            //printing employee
+            myCustomeEmployee.PrintEmployee();
+            Console.WriteLine("\n");
+            //changing name
+            myCustomeEmployee.ChangeName();
+            Console.WriteLine("\n");
+            //chaning salary
+            myCustomeEmployee.ChangeSalary();
+            Console.WriteLine("\n");
+            //printing again
+            myCustomeEmployee.PrintEmployee();
+        }
+    }
+}
